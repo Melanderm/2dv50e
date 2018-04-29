6f15e7c0560baa936c8d24c60c3b883f160fa19e
melanderm <mm222ev@student.lnu.se>
2018-04-29 09:30:09 +0200

    Implementation update

--- code/server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const projectRoutes = require('./project_routes');
-const usersRoutes = require('./users_routes');
-
-module.exports = function(app, db) {
-  projectRoutes(app, db);
-  usersRoutes(app, db);
-};
